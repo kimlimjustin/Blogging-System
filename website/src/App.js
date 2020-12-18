07917b52950e555bd99cd263f058a5f3ce3c3068
kimlimjustin <kimlimjustin@gmail.com>
2020-12-18 16:52:59 +0700

    Initial commit

---
+++ website/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const App = () => (
+    <Router>
+        <h1>Hello World</h1>
+    </Router>
+)
+
+export default App;
