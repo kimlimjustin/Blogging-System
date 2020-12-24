e01f4e8b80c92fc80191681ce8a8890114bd8ee8
kimlimjustin <kimlimjustin@gmail.com>
2020-12-24 20:14:45 +0700

    Website done

--- website/src/Components/index.js
+++ website/src/Components/index.js
@@ -1,10 +1,12 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Cookies from "universal-cookie";
 import getUserByToken from "../Lib/getUserByToken";
 
 const Home = () => {
     const [userInfo, setUserInfo] = useState(null);
+    const [blogs, setBlogs]  = useState(null);
     useEffect(() => {
         const token = new Cookies().get('token');
         getUserByToken(token).then(res => {
@@ -15,12 +17,15 @@ const Home = () => {
         })
         Axios
           .get(`${process.env.REACT_APP_SERVER_URL}/blogs/get/all`)
-          .then(res => console.log(res.data))
+          .then(res => setBlogs(res.data))
           .catch(err => console.error(err));
     }, [])
     return(
         <div className="container">
-            
+            <h1>Recent blogs:</h1>
+            {blogs && blogs.map(blog => {
+                return <Link to = {`/post/${blog._id}`}  key ={blog._id}><div className="col-4 theme-reverse m-2"><div className=" box box-shadow">{blog.title}</div></div></Link>
+            })}
         </div>
     )
 }
